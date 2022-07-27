292e13b176ec411389f36fe13a4263c5838b00f0
pahaze <pahaze200@protonmail.com>
2022-07-27 01:41:05 -0400

    Update to allow text file usage and saving of images

--- GCImageGrabber.py
+++ GCImageGrabber.py
@@ -1,17 +1,62 @@
 #!/usr/bin/env python3
 import argparse
-import json
+import pathlib
 import requests
+import urllib.request
 
 parser = argparse.ArgumentParser(description='GoComics Comic Image Grabber')
-parser.add_argument("--url", help="URL to use")
+parser.add_argument("-u", "--url", help="Use a single URL")
+parser.add_argument("-us", "--urls", nargs="+", help="Use multiple URLs")
+parser.add_argument("-ti", "--txtin", help="Use a text file with URLs")
+parser.add_argument("-to", "--txtout", help="Put the image URLs in a text file")
+parser.add_argument("-s", "--save", default=False, action="store_true", help="Save the images")
+parser.add_argument("-d", "--directory", help="Directory to store images if saving them")
 args = parser.parse_args()
 
-if args.url != None:
-    url = args.url
+notCheckedIfOK = True
+
+def getResponse(url):
+    global notCheckedIfOK
+    if args.txtout != None and args.txtin != None and notCheckedIfOK:
+        if args.txtin == args.txtout:
+            notCheckedIfOK = False
+            print("Input and output text files can not be the same! Ignoring...")
+
     response = requests.get(url=url).text
 
     start = response.find('https://assets.amuniversal.com')
-    print(response[start:].partition('"')[0])
+    link = response[start:].partition('"')[0]
+    print(f'Image URL for {url} is {link}')
+
+    # I don't really know a better way to do this
+    fn = link[link.find('m/') + 2:]
+    
+    if args.txtout != None:
+        if args.txtin != args.txtout:
+            with open(args.txtout, 'a') as file:
+                file.write(f'{url}: {link}\n')
+    
+    if args.save:
+        if args.directory != None:
+            path = pathlib.Path(args.directory)
+            path.mkdir(parents=True, exist_ok=True)
+            urllib.request.urlretrieve(link, f'{args.directory}/{fn}.gif')
+        else:
+            urllib.request.urlretrieve(link, f'{fn}.gif')
+
+
+if args.url != None:
+    getResponse(args.url)
+elif args.urls != None:
+    for url in args.urls:
+        getResponse(url)
+elif args.txtin != None:
+    with open(args.txtin) as file:
+        lines = file.readlines()
+        for line in lines:
+            url = line.rstrip('\n')
+            getResponse(url)
+        
+        file.close()
 else:
     print(parser.print_help())
--- README.md
+++ README.md
@@ -7,11 +7,24 @@ Running is simple, but first you need the following libraries. They're all insta
 argparse
 requests
 ```
+
 After all of that, all you need to do is run the app and supply the URL to the comic. \
 `./GCImageGrabber.py --url https://gocomics.com/comic/date`
 
-# Plans
-Support for saving to a txt file and multiple URL support.
+Or, if you have multiple, you can use more than one too. \
+`./GCImageGrabber.py --urls https://gocomics.com/comic/date https://gocomics.com/another-comic/date`
+
+Or, if you even want, use a .txt file! \
+`./GCImageGrabber.py --txtin path/to/list.txt`
+
+Want to save the URLs in a .txt file? You can do that. \
+`./GCImageGrabber.py --txtout path/to/output-list.txt`
+
+Want to save the pictures? You can do that too. \
+`./GCImageGrabber.py --url https://gocomics.com/comic/date --save`
+
+.. As well as to a certain directory. \
+`./GCImageGrabber.py --url https://gocomics.com/comic/date --save --directory path/to/save/to`
 
 # Legal
 If it's needed, if GoComics wants this taken down for whatever reason, I'm not gonna fight it. This app is free, open source, and non-profitable, however, and licensed under GPLv3.
